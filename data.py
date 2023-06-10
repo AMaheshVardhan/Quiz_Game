1f659ffec701cae7cbaf585f645e699765a3a3e1
Mahesh-Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-06-10 20:59:51 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+question_data = [
+    {"text": "Mount Everest is the tallest mountain in the world.", "answer": "True"},
+    {"text": "Cats can see in complete darkness.", "answer": "False"},
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"},
+    {"text": "The human body has 206 bones.", "answer": "True"},
+    {"text": "Bananas are berries.", "answer": "True"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "Elephants can't jump.", "answer": "True"},
+    {"text": "The currency of India is the Dinar.", "answer": "False"},
+    {"text": "Water boils at 100 degrees Fahrenheit.", "answer": "False"},
+    {"text": "The sun is a star.", "answer": "True"},
+    {"text": "Spiders are insects.", "answer": "False"},
+    {"text": "The Statue of Liberty was a gift from France to the USA.", "answer": "True"}
+]
+
